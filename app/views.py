249be6d20cfea873e7473a0852a0813e29c9f605
Kiddo12175 <sravani.c1992@gmail.com>
2023-11-21 18:33:00 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+ 
+def sandy(request):
+    
+    return render(request,'sandy.html')
+
+def nandu(request):
+    return render(request,'nandu.html')
